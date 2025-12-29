512ff5cc0ca7353979666d031782b2cfbe5c4cda
Raaz <raazraz621@gmail.com>
2025-12-29 13:59:34 +0530

    app.js created, seperated server.js into app.js

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const logger = require('./middlewares/logger');
+
+const app = express();
+
+app.use(logger);
+app.use(express.json);
+
+// routes
+const userRoutes = require('./routes/userRoutes');
+const statusRoutes = require('./routes/statusRoutes');
+
+app.use('/api', userRoutes);
+app.use('/api', statusRoutes);
+
+//error handler MUST be last
+const errorHandler = require('./middlewares/errorHandler');
+app.use(errorHandler);
+
+module.exports = app;
